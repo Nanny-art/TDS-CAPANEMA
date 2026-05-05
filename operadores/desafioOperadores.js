2bfbac43fdbca5b87573578f8591796075a0fc57
nanny.art <elianedutra938@gmail.com>
2026-05-04 21:59:48 -0300

    Atualização

---
+++ operadores/desafioOperadores.js
@@ -0,0 +1,23 @@
+// DESAFIO
+
+/*
+Atalho: SHIFT = ALT + A
+
+
+01. Utilizando os operadores relacionnados, declare 2 vriáveis e
+ imprima no output/console 5 valores TRUE e 5 valores FALSE.
+
+
+*/
+let a = 10;
+let b = 20;
+
+// = atribuição
+// == compara somente o valor
+// === compara valor e tipo
+
+console.log(a =a);
+console.log(a ==b);
+console.log(a == b);
+console.log(a === b);
+console.log(b === a);
